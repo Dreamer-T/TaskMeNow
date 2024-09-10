361a6c48b9a044e4de1733591a67276f066a0c73
T <1727648092@qq.com>
2024-09-11 02:32:37 +1000

    set up task page and the task creation

--- lib/stuff.dart
+++ lib/stuff.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:grouptodo/task.dart';
+
+enum AuthorityGroup { cleaning, manager, painter, plummer }
+
+int ID = 1;
 
 /// 定义成员类
 class Task {
   final int id;
   final String description;
-  final Image role;
+  final Image? image;
+  final List<AuthorityGroup> groupAuthority;
 
   // 使用默认的图片
   Task({
     required this.id,
     required this.description,
-    this.role =
-        const Image(image: AssetImage('assets/default_role.png')), // 这里提供一个默认图片
+    required this.groupAuthority,
+    this.image, // 这里提供一个默认图片
   });
+  String toString() {
+    return "id: " + id.toString() + " description: " + description;
+  }
 }
 
 /// Admin Screen which allows admin to modify group members
@@ -26,7 +35,22 @@ class _StuffScreenState extends State<StuffScreen> {
   int _currentIndex = 0;
 
   // 成员列表
-  List<Task> _members = [];
+  List<Task> _tasks = [];
+  late Task _taskResult;
+  Future<void> _navigateToTaskScreen() async {
+    // 等待 TaskScreen 返回的结果
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TaskScreen()),
+    );
+    // 检查是否返回了数据
+    if (result != null) {
+      setState(() {
+        _taskResult = result;
+        _tasks.add(_taskResult);
+      });
+    }
+  }
 
   // Page list with styles
   List<Widget> get _pages {
@@ -41,14 +65,43 @@ class _StuffScreenState extends State<StuffScreen> {
     return Scaffold(
       body: Container(
         color: Color(0x103237E4),
-        child: ListView.builder(
-          itemCount: _members.length,
+        child: ReorderableListView.builder(
+          itemCount: _tasks.length,
+          padding: EdgeInsets.symmetric(vertical: 8.0), // 设置整体的 padding
+          onReorder: (int oldIndex, int newIndex) {
+            setState(() {
+              if (newIndex > oldIndex) {
+                newIndex -= 1;
+              }
+              final Task movedTask = _tasks.removeAt(oldIndex);
+              _tasks.insert(newIndex, movedTask);
+            });
+          },
           itemBuilder: (context, index) {
-            final member = _members[index];
-            return ListTile(
-              leading: Icon(Icons.person),
-              title: Text('${member.description} (ID: ${member.id})'),
-              subtitle: Text('Group: ${member.role}'),
+            final task = _tasks[index];
+            return Container(
+              key: ValueKey(task.id), // 为 ReorderableListView 提供唯一 key
+              margin: const EdgeInsets.symmetric(
+                  horizontal: 8, vertical: 4.0), // 设置项目之间的间隔
+              child: Material(
+                color: Colors.transparent,
+                elevation: 4.0, // 设置阴影的高度
+                borderRadius: BorderRadius.circular(8.0), // 圆角
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // 设置为统一的背景色
+                    borderRadius: BorderRadius.circular(8.0), // 圆角
+                  ), // 设置项目之间的间隔,
+                  child: ListTile(
+                    leading: Icon(Icons.drag_handle),
+                    title: Text('ID: ${task.id}'),
+                    subtitle: Text('Specified: ${task.description}'),
+                    onTap: () {
+                      print('${task.description}');
+                    },
+                  ),
+                ),
+              ),
             );
           },
         ),
@@ -70,22 +123,13 @@ class _StuffScreenState extends State<StuffScreen> {
     });
   }
 
-  /// 构建新的任务页面
-  Widget _buildNewTaskPage() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('New Task'),
-      ),
-      body: Column(),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Stuff Screen'),
         backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
         elevation: 0,
         titleTextStyle: TextStyle(
           fontWeight: FontWeight.bold,
@@ -94,31 +138,32 @@ class _StuffScreenState extends State<StuffScreen> {
       ),
       body: _pages[_currentIndex],
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 导航到新的任务页面
-          Navigator.pushNamed(
-            context,
-            '/home/stuff/task',
-          );
-        },
+        onPressed: _navigateToTaskScreen,
         backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.deepPurple[50],
+        shape: CircleBorder(),
         child: Icon(Icons.add),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(), // 留出FAB的凹槽
-        notchMargin: 8.0,
+        notchMargin: 6,
+        color: Color(0xFFE0DBEE),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
+          children: [
             IconButton(
               icon: Icon(Icons.task),
+              tooltip: "Ongoing Task",
               onPressed: () {
                 _onItemTapped(0); // 切换到Task页面
               },
             ),
+            // Spacer(),
             IconButton(
               icon: Icon(Icons.archive),
+              tooltip: "Completed Task",
               onPressed: () {
                 _onItemTapped(1); // 切换到Archive页面
               },
--- lib/task.dart
+++ lib/task.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:grouptodo/stuff.dart';
 import 'package:image_picker/image_picker.dart';
 
 class TaskScreen extends StatefulWidget {
@@ -9,37 +10,201 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
+  bool _isModified = false; // 用于检测是否有修改
+  String _taskDescription = '';
+  Image? _selectedImage;
+  List<bool> _checkboxValues = [false, false, false, false]; // 4个多选框的值
   final ImagePicker _picker = ImagePicker();
-  XFile? _image;
+  List<CheckboxListTile> get grouplists {
+    return [
+      // 多选框
+      CheckboxListTile(
+        title: Text('Cleaning'),
+        value: _checkboxValues[0],
+        onChanged: (bool? value) {
+          setState(() {
+            _checkboxValues[0] = value ?? false;
+            _isModified = true; // 多选框被修改
+          });
+        },
+      ),
+      CheckboxListTile(
+        title: Text('Manager'),
+        value: _checkboxValues[1],
+        onChanged: (bool? value) {
+          setState(() {
+            _checkboxValues[1] = value ?? false;
+            _isModified = true;
+          });
+        },
+      ),
+      CheckboxListTile(
+        title: Text('Painter'),
+        value: _checkboxValues[2],
+        onChanged: (bool? value) {
+          setState(() {
+            _checkboxValues[2] = value ?? false;
+            _isModified = true;
+          });
+        },
+      ),
+      CheckboxListTile(
+        title: Text('Plummer'),
+        value: _checkboxValues[3],
+        onChanged: (bool? value) {
+          setState(() {
+            _checkboxValues[3] = value ?? false;
+            _isModified = true;
+          });
+        },
+      ),
+    ];
+  }
 
-  Future<void> _takePhoto() async {
-    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
-    setState(() {
-      _image = image;
-    });
+  Task NewTask() {
+    List<AuthorityGroup> authorityGroup = List.empty(growable: true);
+    if (_checkboxValues[0]) {
+      authorityGroup.add(AuthorityGroup.cleaning);
+    }
+    if (_checkboxValues[1]) {
+      authorityGroup.add(AuthorityGroup.manager);
+    }
+    if (_checkboxValues[2]) {
+      authorityGroup.add(AuthorityGroup.painter);
+    }
+    if (_checkboxValues[3]) {
+      authorityGroup.add(AuthorityGroup.plummer);
+    }
+
+    return Task(
+        id: ID++,
+        description: _taskDescription,
+        groupAuthority: authorityGroup);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Camera Example'),
+        title: Text('New Task'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            _onWillPop(); // 检测用户是否即将放弃修改
+          },
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image != null
-                ? Image.file(File(_image!.path))
-                : Text('No image selected.'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _takePhoto,
-              child: Text('Take Photo'),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // 任务描述输入框
+              TextField(
+                decoration: InputDecoration(labelText: 'Task Description'),
+                onChanged: (value) {
+                  setState(() {
+                    _taskDescription = value;
+                    _isModified = true; // 任务描述被修改
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+
+              // 图片选择按钮
+              GestureDetector(
+                onTap: _selectImage,
+                child: _selectedImage ??
+                    Container(
+                      color: Colors.grey[200],
+                      height: 150,
+                      width: double.infinity,
+                      child: Center(child: Text('Select an Image')),
+                    ),
+              ),
+              SizedBox(height: 20),
+              grouplists[0],
+              grouplists[1],
+              grouplists[2],
+              grouplists[3],
+              SizedBox(height: 20),
+
+              // 保存和放弃按钮
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _onWillPop(); // 放弃修改，检测是否有修改
+                    },
+                    child: Text('Discard'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // 保存任务逻辑
+                      setState(() {
+                        _isModified = false; // 保存后，修改标志重置
+                      });
+
+                      Navigator.pop(context, NewTask()); // 返回上一页面
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple,
+                        foregroundColor: Colors.white),
+                    child: Text('Save'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  // 选择图片的方法
+  Future<void> _selectImage() async {
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery, // 或者使用 ImageSource.camera
+    );
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = Image.file(File(pickedFile.path));
+        _isModified = true; // 图片被修改
+      });
+    }
+  }
+
+  // 用户点击返回或放弃时的提示
+  void _onWillPop() {
+    if (_isModified) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Unsaved Changes'),
+            content: Text(
+                'You have unsaved changes. Do you really want to discard them?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // 关闭对话框
+                },
+                child: Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // 关闭对话框
+                  Navigator.of(context).pop(); // 返回上一页面
+                },
+                child: Text('Discard'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      Navigator.of(context).pop(); // 没有修改时直接返回
+    }
   }
 }
